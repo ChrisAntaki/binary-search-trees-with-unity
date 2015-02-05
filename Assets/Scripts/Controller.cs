b05908ea9aa8d067e89f755129c55dc69804b96b
Chris Antaki <ChrisAntaki@gmail.com>
2015-02-04 21:46:36 -0800

    Simple tree

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controller : MonoBehaviour {
+
+	public GameObject prefabForNode;
+
+	Node CreateNode () {
+		var node = Instantiate (prefabForNode) as GameObject;
+		return node.GetComponent<Node> ();
+	}
+
+	Node CreateNodeFromArray (int[] numbers, int start, int end, Node parent) {
+		// Base case
+		if (start > end) {
+			return null;
+		}
+
+		var middleIndex = (start + end) / 2;
+		var middleValue = numbers [middleIndex];
+
+		print ("Middle Index: " + middleIndex);
+
+		var node = CreateNode ();
+		node.value = middleValue;
+		node.parent = parent;
+
+		// Position the Node;
+		node.UpdatePosition ();
+
+		// Split numbers array
+		node.left = CreateNodeFromArray (numbers, start, middleIndex - 1, node);
+		node.right = CreateNodeFromArray (numbers, middleIndex + 1, end, node);
+
+		return node;
+	}
+
+	// Use this for initialization
+	void Start () {
+		int[] numbers = new int[] {
+			3, 4, 5
+			//1, 2, 3, 4, 5, 6, 7
+		};
+
+		var root = CreateNodeFromArray (numbers, 0, numbers.Length - 1, null);
+	}
+	
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Node : MonoBehaviour {
+
+	public Node root;
+	public Node parent;
+	public Node left;
+	public Node right;
+	public int value;
+
+	public int GetDepth () {
+		var depth = 0;
+		var node = this;
+		while (node.parent) {
+			node = node.parent;
+			depth++;
+		}
+		return depth;
+	}
+
+	public void UpdatePosition () {
+		var depth = GetDepth ();
+		transform.position = new Vector3 (value, 5f - 1.1f * depth, 0);
+	}
+
+}
